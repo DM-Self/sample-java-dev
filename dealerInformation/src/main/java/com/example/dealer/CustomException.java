e7eb3c4a6d6e9d515f39ea90e36c25d7e1eeaeb9
MANIYAR Dastgeer <dastgeer.maniyar@soprabanking.com>
2021-06-05 13:03:14 +0530

    Changes

--- dealerInformation/src/main/java/com/example/dealer/CustomException.java
+++ dealerInformation/src/main/java/com/example/dealer/CustomException.java
@@ -3,7 +3,7 @@ package com.example.dealer;
 
 	public class CustomException extends Exception {
 	    public CustomException() {
-	        super("Problem to execute API might be record not available");
+	        super("Problem  to execute API might be record not available");
 	    }
 	}
 
